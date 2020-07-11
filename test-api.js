d5244fe28defc4dabc0934065c9f6051f626d671
Victor Cavalcante <68138190+tecvictor@users.noreply.github.com>
2020-07-11 12:33:03 -0300

    Delete test-api.js
    this is not necessary

--- test-api.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-const url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='07-07-2020'&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao"
-
-
-axios
-.get(url)
-.then (res => console.log(res.data.value[0].cotacaoVenda))
-
